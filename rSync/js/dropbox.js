9e253ca55abebed2c099cbded39c0bcd3d8c9110
syrajendra <rajendra.sy@gmail.com>
2013-09-10 14:08:35 +0530

    code updated

---
+++ rSync/js/dropbox.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function load_dropbox(cloud_name) {
+	alert('drop box func' + cloud_name);
+}
--- rSync/js/google_drive.js
+++ rSync/js/google_drive.js
@@ -1,13 +1,6 @@
 "use strict";
 
-$(function() {
-	// monitor href clicks
-	$('a[href]').click(function(event) {
-		list_contents(this.name);
-		//list_contents_dummy(this.name);
-	});
-});
-
+var g_cloud_name = '';
 function toggle_init () {
 	$(".icon-minus, .icon-plus").on('click', function(event) {
 	    $(this).toggleClass("icon-minus icon-plus");	     
@@ -15,45 +8,15 @@ function toggle_init () {
 }
 
 /* Function  for testing purpose */
-function list_contents_dummy() {
-	var list = [
-		{'title' : 'One', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '1'},
-		{'title' : 'Two', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '2'},
-		{'title' : 'Three', 'mimeType' : 'application/vnd.google-apps.file', 'id' : '3'},
-		{'title' : 'Four', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '4'}
-	];
-	handle_google_drive_file_list(list);
-}
-
-function list_contents(cloud_name) {
-	var popup = window.open("about:blank", "dummy_window", "height=50, width=50", true);
-	alert(popup);
-	setTimeout(function() {
-    	if(!popup || popup.outerHeight === 0) {
-    		alert("Popup Blocker is enabled! Please unblock Popup for this site");
-    		return;
-        } else {
-        	popup.close()        	
-        }
-    }, 5);
-
-	switch(cloud_name) {
-		case "googledrive": 
-					load_google_drive();
-					break;
-		case "dropbox": 
-					alert("Still not implemented");
-					break;
-		case "copy": 
-					alert("Still not implemented");
-					break;
-		case "icloud": 
-					alert("Still not implemented");
-					break;
-		default:
-					alert("Not supported !!!");					
-	}
-}
+//function list_contents() {
+//	var list = [
+//		{'title' : 'One', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '1'},
+//		{'title' : 'Two', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '2'},
+//		{'title' : 'Three', 'mimeType' : 'application/vnd.google-apps.file', 'id' : '3'},
+//		{'title' : 'Four', 'mimeType' : 'application/vnd.google-apps.folder', 'id' : '4'}
+//	];
+//	handle_google_drive_file_list(list);
+//}
 
 function handle_google_drive_auth_result(resp) {
 	if (resp && !resp.error) { 			 		
@@ -62,7 +25,7 @@ function handle_google_drive_auth_result(resp) {
  		console.log("rSync: Calling authorization popup !!!")
  		authorize_google_drive(false);
  	}
- 	var folder_div = document.getElementById('folder_list');
+ 	var folder_div = document.getElementById('folder_' + g_cloud_name);
  	list_google_drive_contents('root', folder_div);
  	toggle_init();
 }
@@ -82,53 +45,17 @@ function authorize_google_drive(immediate) {
 	handle_google_drive_auth_result);
 }
 
-function load_google_drive() {	
+function load_google_drive(cloud_name) {
+	g_cloud_name = cloud_name;
 	gapi.load('auth:client,drive-realtime,drive-share', function() {
 		authorize_google_drive(true); // without pop up
 	});
 }
 
-function create_radio_button(label, name, radio_style, parent) {
-	var elem = document.createElement('input');
-	elem.setAttribute('type', 'radio');
-	elem.setAttribute('name', name);
-	elem.setAttribute('style', radio_style);
-	var lab = document.createElement('label');
-	lab.setAttribute('class', 'radio inline')
-	var text = document.createTextNode(label);
-	lab.appendChild(text);	    	
-	parent.appendChild(elem);
-	parent.appendChild(lab);
-	parent.appendChild(document.createElement('br'));
-}
-
-function create_button_mini(name, value, parent) {
-	var elem = document.createElement('input');
-	elem.setAttribute('type', 'button');
-	elem.setAttribute('value', value);
-	elem.setAttribute('name', name);
-	elem.setAttribute('class', 'btn btn-mini');
-	parent.appendChild(elem);			
-}
-
-function create_div(id, classname, text, style, parent) {
-	var elem = document.createElement('div');
-	elem.id = id;
-	elem.className = classname;
-	elem.textContent = text;
-	elem.setAttribute('style', style);
-	parent.appendChild(elem);	
-}
-
-function create_icon(id, target, parent) {
-	var html_icon = '<i style="margin-top:0.1cm;" class="icon-plus" data-toggle="collapse" data-target="#' + target  + '" id="img_' + id + '"></i>';
-	parent.insertAdjacentHTML('beforeend', html_icon);
-}
-
-var g_count = 0;
+var gdrive_count = 0;
 function get_collapse_unique_target_id(folder_name) {
-	g_count++;
-	var target_id = 'target_' + folder_name + "_" + g_count;
+	gdrive_count++;
+	var target_id = 'target_' + folder_name + "_" + gdrive_count;
 	return target_id;
 }
 
